40367152f04c4e09c6aa989f7c46090e04338802
xueyayang <xueyayang@gmail.com>
2014-05-13 17:24:20 +0800

    1. Decouples the format-output to a single function

--- gaussian-2d/gaussian-2d.py
+++ gaussian-2d/gaussian-2d.py
@@ -26,22 +26,28 @@ def gaussian_2d(size):
 		for x in range(-size, size + 1, 1):
 			b = pow(x,2) + pow(y,2)
 			G = a*math.exp(-b/c)
-			if(y == 0 and x == 0):
-				print(bcolors.OKGREEN),
-				print("%.8f" % round(G,8)),
-				print(bcolors.ENDC),
-			elif math.fabs(y) == 3 and math.fabs(x) == 1:
-				print(bcolors.OKGREEN),
-				print("%.8f" % round(G,8)),
-				print(bcolors.ENDC),
-			elif math.fabs(y) == 1 and math.fabs(x) == 3:
-				print(bcolors.OKGREEN),
-				print("%.8f" % round(G,8)),
-				print(bcolors.ENDC),
-			else:
-				print(" %.8f " % round(G,8)),
-
+			format_output(x,y,G)
 		print '\n'
+
+# utility print, ensure:
+# 1. colorful some value
+# 2. align the number
+def format_output(x,y,G):
+	if(y == 0 and x == 0):
+		print(bcolors.OKGREEN),
+		print("%.8f" % round(G,8)),
+		print(bcolors.ENDC),
+	elif math.fabs(y) == 3 and math.fabs(x) == 1:
+		print(bcolors.OKGREEN),
+		print("%.8f" % round(G,8)),
+		print(bcolors.ENDC),
+	elif math.fabs(y) == 1 and math.fabs(x) == 3:
+		print(bcolors.OKGREEN),
+		print("%.8f" % round(G,8)),
+		print(bcolors.ENDC),
+	else:
+		print(" %.8f " % round(G,8)),
+
 
 if __name__=="__main__":
 	gaussian_2d(3);
