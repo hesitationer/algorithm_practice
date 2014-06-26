0ceb7320032c1b44dfdad17e491e7f358c217efd
xueyayang <xueyayang@gmail.com>
2014-06-26 20:07:25 +0800

    1. Opencv_sample 	- Sample files copy from the opencv source files
    2. src/harris_corners.cpp
    	- Complete the harris_corners detected.
    
    3. test/test_case.cpp
    	- Add some case for operate cv::Mat

--- harris_corner/src/harris_corner.cpp
+++ harris_corner/src/harris_corner.cpp
@@ -9,106 +9,96 @@ int find_corners(Mat &image)
 	int width = image.cols;
 	int height = image.rows;
 
-	Mat show_corners = Mat::zeros(height,width,CV_8UC3);
+	Mat diff_x  = Mat::zeros(height,width,CV_32FC1); 
+	Mat diff_y  = Mat::zeros(height,width,CV_32FC1); 
 
-	Mat diff_x  = Mat::zeros(height,width,CV_8UC1); 
-	Mat diff_y  = Mat::zeros(height,width,CV_8UC1); 
+	Mat diff_xx = Mat::zeros(height,width,CV_32FC1); 
+	Mat diff_yy = Mat::zeros(height,width,CV_32FC1);
+	Mat diff_xy = Mat::zeros(height,width,CV_32FC1); 
 
-	Mat diff_xx = Mat::zeros(height,width,CV_16UC1); 
-	Mat diff_yy = Mat::zeros(height,width,CV_16UC1);
-	Mat diff_xy = Mat::zeros(height,width,CV_16UC1); 
-
-	Mat corner_map = Mat::zeros(height,width,CV_16UC1);
+	Mat corner_map = Mat::zeros(height,width,CV_32FC1);
 
 	// range: [1, w-1], [1, h - 1]
 	for(int i = 1; i < height - 1; ++i){
 		for(int j =1; j < width - 1; ++j){
-			*diff_x.ptr(i,j) =  *image.ptr(i,j + 1) - *image.ptr(i,j-1);
-			*diff_y.ptr(i,j) =  *image.ptr(i + 1,j) - *image.ptr(i - 1,j);
+			diff_x.at<float>(i,j) =  image.at<unsigned char>(i,j + 1) - image.at<unsigned char>(i,j-1);
+			diff_y.at<float>(i,j) =  image.at<unsigned char>(i + 1,j) - image.at<unsigned char>(i - 1,j);
 		}
 	}
 
 	for(int i = 1; i < height - 1; ++i){
 		for(int j = 1; j < width - 1; ++j){
-			*(unsigned short*)diff_xx.ptr(i,j) = pow(*diff_x.ptr(i,j),2);
-			*(unsigned short*)diff_yy.ptr(i,j) = pow(*diff_y.ptr(i,j),2);
+			diff_xx.at<float>(i,j) = pow(diff_x.at<float>(i,j),2);
+			diff_yy.at<float>(i,j) = pow(diff_y.at<float>(i,j),2);
 
-			*(unsigned short*)diff_xy.ptr(i,j) = (*diff_x.ptr(i,j)) * ( *diff_y.ptr(i, j));
+			diff_xy.at<float>(i,j) = diff_x.at<float>(i,j) * diff_y.at<float>(i,j);
 		}
 	}
 
+
+	// smooth the diff_xx, diff_yy, diff_xy
 	// Gaussian  model
 	//
-	//   1  2  1
-	//   2  4  2
-	//   1  2  1
-	unsigned short max = 0;
+	// boxFilter:
+	//
+	//    1 1 1
+	//    1 1 1
+	//    1 1 1
+	boxFilter(diff_xx,diff_xx,diff_xx.depth(), Size(2,2),
+			Point(-1,-1),false,BORDER_DEFAULT);
+	boxFilter(diff_yy,diff_yy,diff_yy.depth(), Size(2,2),
+			Point(-1,-1),false,BORDER_DEFAULT);
+	boxFilter(diff_xy,diff_xy,diff_xy.depth(), Size(2,2),
+			Point(-1,-1),false,BORDER_DEFAULT);
+	
+
+	// calcHarris
+	float max = 0.0;
 	for(int i = 1; i < height - 1; ++i){
 		for(int j = 1; j < width - 1; ++j){
 
-			Mat H = Mat::zeros(2,2,CV_16UC1);
+			float h00 = diff_xx.at<float>(i,j);
+			float h01 = diff_xy.at<float>(i,j); float h10 = h01;
+			float h11 = diff_yy.at<float>(i,j);
 
-			// size of scan window is 3
-			unsigned short h00 = *(unsigned short*)H.ptr(0,0) = 
-			   (*(unsigned short*)diff_xx.ptr(i - 1,j)*2 + *(unsigned short*)diff_xx.ptr(i - 1,j - 1)*1 + *(unsigned short*)diff_xx.ptr(i - 1,j+1)*1 +
-				*(unsigned short*)diff_xx.ptr(i + 0,j)*4 + *(unsigned short*)diff_xx.ptr(i + 0,j - 1)*2 + *(unsigned short*)diff_xx.ptr(i + 0,j+1)*2 +
-				*(unsigned short*)diff_xx.ptr(i + 1,j)*2 + *(unsigned short*)diff_xx.ptr(i + 1,j - 1)*1 + *(unsigned short*)diff_xx.ptr(i + 1,j+1)*1)/16.0;
-
-			unsigned short h01 = *(unsigned short*)H.ptr(0,1) = 
-			   (*(unsigned short*)diff_xy.ptr(i - 1,j)*2 + *(unsigned short*)diff_xy.ptr(i - 1,j - 1)*1 + *(unsigned short*)diff_xy.ptr(i - 1,j+1)*1 +
-				*(unsigned short*)diff_xy.ptr(i + 0,j)*4 + *(unsigned short*)diff_xy.ptr(i + 0,j - 1)*2 + *(unsigned short*)diff_xy.ptr(i + 0,j+1)*2 +
-				*(unsigned short*)diff_xy.ptr(i + 1,j)*2 + *(unsigned short*)diff_xy.ptr(i + 1,j - 1)*1 + *(unsigned short*)diff_xy.ptr(i + 1,j+1)*1)/16.0;
-
-			//float h10 = *H.ptr(1,0) = (float)
-			//	(*diff_xy.ptr(i - 1,j)*2 + *diff_xy.ptr(i - 1,j - 1)*1 + *diff_xy.ptr(i - 1,j+1)*1 +
-			//	*diff_xy.ptr(i + 0,j)*4 + *diff_xy.ptr(i + 0,j - 1)*2 + *diff_xy.ptr(i + 0,j+1)*2 +
-			//	*diff_xy.ptr(i + 1,j)*2 + *diff_xy.ptr(i + 1,j - 1)*1 + *diff_xy.ptr(i + 1,j+1)*1)/16;
-			
-			unsigned short h10 = h01;
-
-			unsigned short h11 = *(unsigned short*)H.ptr(1,1) = 
-			   (*(unsigned short*)diff_yy.ptr(i - 1,j)*2 + *(unsigned short*)diff_yy.ptr(i - 1,j - 1)*1 + *(unsigned short*)diff_yy.ptr(i - 1,j+1)*1 +
-				*(unsigned short*)diff_yy.ptr(i + 0,j)*4 + *(unsigned short*)diff_yy.ptr(i + 0,j - 1)*2 + *(unsigned short*)diff_yy.ptr(i + 0,j+1)*2 +
-				*(unsigned short*)diff_yy.ptr(i + 1,j)*2 + *(unsigned short*)diff_yy.ptr(i + 1,j - 1)*1 + *(unsigned short*)diff_yy.ptr(i + 1,j+1)*1)/16.0;
-
-			//printf("hxx:%f,%f,%f,%f\n",h00,h01,h10,h11);
-
-			unsigned short H_det = h00*h11 - h01*h10;
-			unsigned short H_trace = h00 + h11;
-			unsigned short small_eigenvalue = (unsigned short)((float)H_det - H_trace*H_trace*0.4);
+			float H_det = h00*h11 - h01*h10;
+			float H_trace = h00 + h11;
+			float small_eigenvalue = (float)(H_det - H_trace*H_trace*0.04);
 
 			if(small_eigenvalue > max){
 				max = small_eigenvalue;
-				printf("max:%d\n",max);
+				printf("max:%f\n",max);
 			}
 
-			*(unsigned short*)corner_map.ptr(i,j) = small_eigenvalue;
+			corner_map.at<float>(i,j) = small_eigenvalue;
 		}
 	}
 
- 	unsigned short corner_epsilon = max*quality_level;
-	printf("corner_epsilon:%d\n",corner_epsilon);
+ 	float corner_epsilon = max*quality_level;
+	printf("corner_epsilon:%f\n",corner_epsilon);
+
+	// Normalizing
+	Mat norm_corner_map, norm_corner_map_scaled;
+	normalize(corner_map,norm_corner_map,0,255,NORM_MINMAX,CV_32FC1,Mat());
+	convertScaleAbs(norm_corner_map,norm_corner_map_scaled);
 
 	for(int i = 0; i < height - 1; ++i){
 		for(int j = 0; j < width - 1; ++j){
-			unsigned short value = corner_map.at<unsigned short>(i,j);
-			//printf("value:%d\n",value);
-			if(value > corner_epsilon){
-				circle( image, Point(j,i), 1, CV_RGB(0,255,0), -1 );			
-				circle( show_corners, Point(j,i), 1, CV_RGB(0,255,0), 1 );			
+
+			int value = (int)norm_corner_map.at<float>(i,j);
+			if(value > 160){
+				circle( norm_corner_map_scaled, Point(j,i), 5, CV_RGB(0,255,0), 2, 8, 0 );			
 			}
 		}
 	}
-
 
 	imshow("diff_x",diff_x);
 	imshow("diff_y",diff_y);
 	imshow("diff_xx",diff_xx);
 	imshow("diff_yy",diff_yy);
 	imshow("diff_xy",diff_xy);
-	imshow("show_corners",show_corners);
 	namedWindow("find_corners",CV_WINDOW_NORMAL);
-	imshow("find_corners",image);
+	imshow("dst_norm_scaled",norm_corner_map_scaled);
 	waitKey(0);
 
 	return 0;
--- harris_corner/test/test_case.cpp
+++ harris_corner/test/test_case.cpp
@@ -7,8 +7,8 @@ extern int find_corners(Mat &image);
 
 ERIC_TEST(harris_corners, find_corners)
 {
-	//Mat image = imread("../test/corners.bmp");
-	Mat image = imread("../test/corners_01.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Mat image = imread("../test/corners.bmp",CV_LOAD_IMAGE_GRAYSCALE);
+	//Mat image = imread("../test/corners_01.jpg",CV_LOAD_IMAGE_GRAYSCALE);
 	//Mat image = imread("../test/corners_01.jpg");
 	//imshow("corner",image);
 	
@@ -18,25 +18,41 @@ ERIC_TEST(harris_corners, find_corners)
 
 ERIC_TEST(harris_corners, float_mat)
 {
-	Mat float_mat = Mat::zeros(2,2,CV_16UC1);
+	Mat short_mat = Mat::zeros(2,2,CV_16UC1);
 
-	float_mat.at<unsigned short>(0,0) = 255*255 + 14;
-	float_mat.at<unsigned short>(0,1) = 255*255 + 16;
-	float_mat.at<unsigned short>(1,0) = 255*255;
-	float_mat.at<unsigned short>(1,1) = 65535;
+	short_mat.at<unsigned short>(0,0) = 255*255 + 14;
+	short_mat.at<unsigned short>(0,1) = 255*255 + 16;
+	short_mat.at<unsigned short>(1,0) = 255*255;
+	short_mat.at<unsigned short>(1,1) = 65535;
 
 	// get by ptr
-	float h00 = *(unsigned short*)float_mat.ptr(0,0);
-	float h01 = *(unsigned short*)float_mat.ptr(0,1);
-	float h10 = *(unsigned short*)float_mat.ptr(1,0);
-	float h11 = *(unsigned short*)float_mat.ptr(1,1);
+	float h00 = *(unsigned short*)short_mat.ptr(0,0);
+	float h01 = *(unsigned short*)short_mat.ptr(0,1);
+	float h10 = *(unsigned short*)short_mat.ptr(1,0);
+	float h11 = *(unsigned short*)short_mat.ptr(1,1);
 
 	// get by at
-	float at00 = float_mat.at<unsigned short>(0,0);
-	float at01 = float_mat.at<unsigned short>(0,1);
-	float at10 = float_mat.at<unsigned short>(1,0);
-	float at11 = float_mat.at<unsigned short>(1,1);
+	float at00 = short_mat.at<unsigned short>(0,0);
+	float at01 = short_mat.at<unsigned short>(0,1);
+	float at10 = short_mat.at<unsigned short>(1,0);
+	float at11 = short_mat.at<unsigned short>(1,1);
 
 	printf("GetByPtr:%f,%f,%f,%f\n",h00,h01,h10,h11);
 	printf("GetByPtr:%f,%f,%f,%f\n",at00,at01,at10,at11);
+
+	Mat float_mat = Mat::zeros(2,2,CV_32FC1);
+	float_mat.at<float>(0,0) = 255 - 241;
+	float_mat.at<float>(0,1) = 241 - 255;
+	int a = (int)float_mat.at<float>(0,1);
+	printf("float_mat(0,0):%f\n",float_mat.at<float>(0,0));
+	printf("float_mat(0,1):%f\n",float_mat.at<float>(0,1));
+	printf("(int)float_mat.at<float>(0,1):%d\n",a);
+}
+
+ERIC_TEST(harris_corners, operators_order)
+{
+	int a = 1;
+	a += 5 - 4; // after executing, a is 2
+
+	printf("a is:%d\n",a);
 }
