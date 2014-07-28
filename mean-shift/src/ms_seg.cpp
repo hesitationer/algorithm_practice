e7d66e14a72438340e528bbacc8c0e03ea36b797
xueyayang <xueyayang@gmail.com>
2014-07-28 18:47:40 +0800

    1. ms_seg.cpp 	- Add the iterate part
    2. .ycm_extra_conf.py
    	- config file for YCM

--- mean-shift/src/ms_seg.cpp
+++ mean-shift/src/ms_seg.cpp
@@ -43,64 +43,79 @@ static void floodFillPostprocess( Mat& img, const Scalar& colorDiff=Scalar::all(
 }
 
 #define RGB_DIS_LEVEL_SQUARE  20*20
+#define MAX_ITERATE_NUM 5 
 void mean_shift_seg(Mat src, Mat dst)
 {
 	int h = src.rows;
 	int w = src.cols;
 
 	int size = 15;
-	// Filter
-	// skip the 2-pixs bound for (3,3)
-	for(int i = size; i < h - size; i++){
-		for(int j = size; j < w - size; j++){
+	int sx = 0, sy = 0;
+	int x0 = 0, y0 = 0;
+	int x1 = 0, y1 = 0;
 
-			
+	unsigned char C[3] = {0};
+	unsigned char temp[3] = {0};
+
+	// Filter
+	for(int i = 0; i < h; i++){
+		for(int j = 0; j < w; j++){
+
 			unsigned char *center = src.data + i*w*3 + j*3;
 			int sum_r = 0, sum_g = 0, sum_b = 0;
-			int good_neighbor = 0;
 
-			if(size == 3){
-				for(int k = 0; k < 8; k++){
+			C[0] = center[0]; C[1] = center[1]; C[2] = center[2]; 
+			sx = 0, sy = 0;
+			x0 = j; y0 = i;
+			for(int iter_count = 0; iter_count < MAX_ITERATE_NUM;iter_count++)
+			{
+				int minx = (x0 - size);  minx = max(0,minx); 
+				int miny = (y0 - size);  miny = max(0,miny); 
+				int maxx = (x0 + size);  maxx = min(maxx,w-1); 
+				int maxy = (y0 + size);  maxy = min(maxy,h-1); 
 
-					// k-th neighbor
-					unsigned char *Nk = src.data + (i+N8[k].y)*w*3 + (j+N8[k].x)*3; 
-					int dis = cal_rgb_dis_square(center,Nk);
-					if(dis < RGB_DIS_LEVEL_SQUARE){
-						sum_r += Nk[0];sum_g += Nk[1];sum_b += Nk[2];
+				int count = 0; int good_neighbors = 0;
+				for(int k = miny; k <= maxy; k++){
+					for(int q = minx; q <= maxx; q++){
 
-						good_neighbor++;
-					}
-				}	
-			}else{
-				for(int k = -size; k <= size; k++){
-					for(int q = -size; q <= size; q++){
-
-						unsigned char *Nk = src.data + (i+k)*w*3 + (j+q)*3; 
-						int dis = cal_rgb_dis_square(center,Nk);
-						if(dis < RGB_DIS_LEVEL_SQUARE){
+						unsigned char *Nk = src.data + k*w*3 + q*3; 
+						int dis = cal_rgb_dis_square(C,Nk);
+						if(dis <= RGB_DIS_LEVEL_SQUARE){
 							sum_r += Nk[0];sum_g += Nk[1];sum_b += Nk[2];
 
-							good_neighbor++;
+							sx += q; sy += k; good_neighbors++;
 						}
 					}
 				}
-			}
+
+				if(good_neighbors == 0)
+					break;
+
+				double icount = 1./ good_neighbors;
+				x1 =    round(sx*icount);
+				y1 =    round(sy*icount);
+				sum_r = round(sum_r*icount);
+				sum_g = round(sum_g*icount);
+				sum_b = round(sum_b*icount);
+
+				int stop_flag;
+				temp[0] = sum_r;temp[1] = sum_g;temp[2] = sum_b;
+			   	stop_flag = (x0 == x1 && y0 == y1) || abs(x1-x0) + abs(y1-y0) + 
+					cal_rgb_dis_square(C,temp) <= 1.0f;
+
+				x0 = x1; y0 = y1;
+				C[0] = sum_r; C[1] = sum_g; C[2] = sum_b; 
+
+				if( stop_flag )
+					break;
+			}// iterate N times
 
 			// mean
 			unsigned char *dst_center = dst.data + i*w*3 + j*3;
-			if(good_neighbor > 0){
-				//printf("mean\n");
-				dst_center[0] = sum_r/good_neighbor;
-				dst_center[1] = sum_g/good_neighbor;
-				dst_center[2] = sum_b/good_neighbor;
-			}else{
-				dst_center[0] = center[0];
-				dst_center[1] = center[1];
-				dst_center[2] = center[2];
-			}
-			
+			dst_center[0] = C[0]; dst_center[1] = C[1]; dst_center[2] = C[2];
+
 		}
-	}// for(int i = 2; i < h; i++)
+	}// for(int i = 0; i < h; i++)
 
 	// color the result
 	floodFillPostprocess(dst,Scalar::all(2));
